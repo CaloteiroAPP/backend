3a228ef0a19aafe648109e3960b75a6b6eb8a339
tiagosora <tiagogcarvalho2002.@>
2024-08-18 19:48:40 +0100

    fix

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,10 +1,9 @@
 
-from typing import List, Optional
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel
 
-from src.models.friend_model import Friend
 from src.models.user_settings_model import UserSettings
 
 
@@ -12,12 +11,9 @@ class User(BaseModel):
     _id: UUID
     email: str
     first_name: str
-    friends: List[Friend] = []
-    friend_requests: List[UUID] = []
     last_name: str
     password: str
     personal_activity: List[UUID] = []
-    phone: Optional[int] = None
     sessions: List[UUID] = []
     user_settings: UserSettings = UserSettings()
 
--- src/models/user_settings_model.py
+++ src/models/user_settings_model.py
@@ -1,15 +1,19 @@
 
 
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 from pydantic import BaseModel
 
+from src.models.friend_model import Friend
 from src.models.notification_settings_model import NotificationSettings
 
 
 class UserSettings(BaseModel):
     app_access_code: Optional[int] = None
     blocked_users: list[UUID] = []
+    friends: List[Friend] = []
+    friend_requests: List[UUID] = []
     muted_users: list[UUID] = []
     notification_settings: NotificationSettings = NotificationSettings()
+    phone: Optional[int] = None
     verified: bool = False
